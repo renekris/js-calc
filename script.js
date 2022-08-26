55d7f11c8b78db19f503b07d125094c4d1fad60b
Renekris <renekrispohlak@gmail.com>
2022-08-26 19:47:33 +0300

    Create add, subtract, multiply & divide functions

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const buttons = document.getElementsByClassName('button');
 const operands = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-const operators = ['clear', 'plus-minus', 'percentage', 'divide', 'multiply', 'subtract', 'add', 'dot', 'equals']
+const operandFunctions = ['clear', 'plus-minus', 'percentage', 'dot']
+const operators = ['divide', 'multiply', 'subtract', 'add', 'equals']
 const numberField = document.getElementById('display');
-let dataEntered = false;
-let currentNumber = 0;
+let firstOperand = null;
+let firstOperator = null;
+let secondOperand = null;
+let secondOperator = null;
+let displayNum = 0;
 
 Array.from(buttons).forEach(element => {
     element.addEventListener('pointerup', e => buttonClicked(e));
@@ -12,66 +16,119 @@ Array.from(buttons).forEach(element => {
 function buttonClicked(e) {
     const input = e.target.getAttribute('data-input');
     if (operands.includes(input)) {
-        if (!dataEntered) {
-            if (input == '0') return
-            dataEntered = true;
-            currentNumber = input;
-        } else if (dataEntered) {
-            currentNumber += input;
+        if (firstOperator === null) {
+            if (firstOperand === null) {
+                if (input == '0') return;
+                firstOperand = input;
+            } else {
+                firstOperand += input;
+            }
+            displayNum = firstOperand;
+        } else if (secondOperator === null) {
+            if (secondOperand === null) {
+                secondOperand = input;
+            } else {
+                secondOperand += input;
+            }
+            displayNum = secondOperand;
         }
         updateDisplay();
     } else if (operators.includes(input)) {
-        calculateData(input);
-    }
+        if (firstOperator === null) {
+            if (input === 'equals') return;
+            firstOperand = displayNum;
+            firstOperator = input;
+        } else {
+            secondOperator = input;
+            if (input === 'equals') {
+                displayNum = operate(firstOperand, secondOperand, firstOperator);
+            } else {
+                clearMemory();
+                firstOperand = displayNum;
+                firstOperator = input;
+            }
+            updateDisplay();
+        }
+    } else if (operandFunctions.includes(input)) operandFunctionSwitch(input);
 };
 
-function calculateData(operator) {
-    switch (operator) {
+function operandFunctionSwitch(operandFunction) {
+    switch (operandFunction) {
         case 'clear':
+            clearMemory();
             clearDisplay();
+            updateDisplay();
             break;
         case 'plus-minus':
-            negateNumber();
+            addNegate()
             updateDisplay();
             break;
         case 'percentage':
-            makePercentage();
+            addPercentage()
             updateDisplay();
             break;
         case 'dot':
             addDot();
             updateDisplay();
             break;
-        default:
-            break;
     }
 }
 
 function clearDisplay() {
-    currentNumber = 0;
-    dataEntered = false;
+    displayNum = 0;
     updateDisplay();
 }
 
-function negateNumber() {
-    if (currentNumber > 0) currentNumber = '-' + currentNumber;
-    else currentNumber = Math.abs(currentNumber);
+function operate(firstNumber, secondNumber, operator) {
+    if (operator === 'add') return parseFloat(firstNumber) + parseFloat(secondNumber);
+    if (operator === 'subtract') return parseFloat(firstNumber) - parseFloat(secondNumber);
+    if (operator === 'multiply') return parseFloat(firstNumber) * parseFloat(secondNumber);
+    if (operator === 'divide') {
+        if (firstNumber === 0 || secondNumber === 0) return 'BZzT! Error!';
+        else return parseFloat(firstNumber) / parseFloat(secondNumber);
+    }
+}
+
+function selectOperator(operator) {
+    // if (isFirstOperand) {
+    //     numTempOne = numCurrent;
+    //     isFirstOperand = false;
+    //     isDataEntered = false;
+    //     updateDisplay();
+    // } else if (!isFirstOperand) {
+    //     numTempTwo = numCurrent;
+    //     isFirstOperand = true;
+    //     isDataEntered = false;
+    //     numCurrent = operate(numTempOne, numTempTwo, operator);
+    //     updateDisplay();
+    // }
+
+}
+
+function clearMemory() {
+    firstOperand = null;
+    firstOperator = null;
+    secondOperand = null;
+    secondOperator = null;
+}
+
+function addNegate() {
+    displayNum = (displayNum * -1).toString();
 }
 
 function updateDisplay() {
-    numberField.innerText = numberWithSpaces(currentNumber);
-    console.log(parseFloat(currentNumber))
-    console.log(currentNumber);
+    numberField.innerText = numberWithSpaces(displayNum);
+    console.log(parseFloat(displayNum))
+    console.log(displayNum);
 }
 
-function makePercentage() {
-    currentNumber = currentNumber / 100;
+function addPercentage() {
+    displayNum = parseFloat(displayNum) / 100;
 }
 
 function addDot() {
-    if (Number.isInteger(parseFloat(currentNumber)) && !currentNumber.toString().includes('.')) {
-        dataEntered = true;
-        currentNumber += '.';
+    if (Number.isInteger(parseFloat(displayNum)) && !displayNum.toString().includes('.')) {
+        displayNum += '.';
     }
 }
 
